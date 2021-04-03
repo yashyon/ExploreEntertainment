05469ec7563dc81198fd3a976c1b79fb08d89dd1
yashyon <yashpradhan60@gmail.com>
2021-04-03 16:39:59 +0530

    Initial Commit

--- app/src/main/java/com/example/exploreentertainment/ui/fragments/movies/MoviesFragment.kt
+++ app/src/main/java/com/example/exploreentertainment/ui/fragments/movies/MoviesFragment.kt
@@ -12,10 +12,13 @@ import com.example.exploreentertainment.ui.fragments.nowplaying.NowPlayingViewMo
 
 class MoviesFragment : Fragment() {
 
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         return inflater.inflate(R.layout.movies_fragment, container, false)
     }
 
--- app/src/main/java/com/example/exploreentertainment/ui/fragments/movies/MoviesViewModel.kt
+++ app/src/main/java/com/example/exploreentertainment/ui/fragments/movies/MoviesViewModel.kt
@@ -3,5 +3,5 @@ package com.example.exploreentertainment.ui.fragments.movies
 import androidx.lifecycle.ViewModel
 
 class MoviesViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
 }
